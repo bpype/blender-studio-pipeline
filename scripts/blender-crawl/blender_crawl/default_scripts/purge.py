72742525a0601d66ab11e90a5b732198b37c28dc
demeterdzadik@gmail.com <demeterdzadik@gmail.com>
2023-07-12 17:45:15 +0200

    Blender Crawl: During Purge Remove Fake User for Linked Data-Blocks (#105)
    ...and collections.
    
    I believe this should be built-in Blender behaviour, but it's already on the [devs' radar](https://projects.blender.org/blender/blender/issues/106321), and in the meantime we have a production that will need to be cleaned up soon.
    
    To explain a bit better, here's what can happen sometimes:
    - You have an ID with a fake user in an asset (usually a collection)
    - You link the asset to a shot
    - You change your mind, and don't want the asset in the shot anymore; You might Un-link it from the Scene instead of "Delete Hierarchy".
    - Now its collections have no real users left
    - You run Purge and expect it to go away from the file
    - It does not, because it has a fake user flag.
    
    Even though obviously that fake user flag's intention was to keep the ID in the **original** file, not when it gets linked in somewhere else. Not to mention that people don't realize that Collections have the fake user flag set to True by default, since it's not visible *anywhere*.
    
    Tested on Pets shots 50_70, 50_80, 50_90, 50_100.
    
    Reviewed-on: https://projects.blender.org/studio/blender-studio-pipeline/pulls/105

--- scripts/blender-crawl/blender_crawl/default_scripts/purge.py
+++ scripts/blender-crawl/blender_crawl/default_scripts/purge.py
@@ -20,5 +20,29 @@
 # (c) 2021, Blender Foundation
 
 import bpy
-bpy.ops.outliner.orphans_purge(do_local_ids=True, do_linked_ids=True, do_recursive=True)
+
+
+def better_purge(context, clear_coll_fake_users=True):
+    """Call Blender's purge function, but first Python-override all library IDs' 
+    use_fake_user to False.
+    Otherwise, linked IDs essentially do not get purged properly.
+
+    Also set all Collections' use_fake_user to False, so unused collections 
+    aren't kept in the file.
+    """
+
+    if clear_coll_fake_users:
+        for coll in bpy.data.collections:
+            coll.use_fake_user = False
+
+    id_list = list(bpy.data.user_map().keys())
+    for id in id_list:
+        if id.library:
+            id.use_fake_user = False
+
+    bpy.ops.outliner.orphans_purge(
+        do_local_ids=True, do_linked_ids=True, do_recursive=True)
+
+
+better_purge()
 bpy.ops.wm.quit_blender()
