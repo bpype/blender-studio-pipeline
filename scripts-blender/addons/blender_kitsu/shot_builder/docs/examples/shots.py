0fa0363d235d17f792cb85bd7616b6420073b1e3
Nick Alberelli <nick@tinymedia.ca>
2023-09-27 14:02:04 +0200

    Blender Kitsu: Fix Shot Builder example `shots.py`

--- scripts-blender/addons/blender_kitsu/shot_builder/docs/examples/shots.py
+++ scripts-blender/addons/blender_kitsu/shot_builder/docs/examples/shots.py
@@ -2,28 +2,40 @@ from blender_kitsu.shot_builder.shot import Shot
 from blender_kitsu.shot_builder.project import Production
 
 
-class SpriteFrightShot(Shot):
+class ProductionShot(Shot):
     def get_anim_file_path(self, production: Production, shot: Shot) -> str:
-        """
-        Get the animation file path for this given shot.
-        """
-        return self.file_path_format.format_map({
-            'production': production,
-            'shot': shot,
-            'task_type': "anim"
-        })
+        """Get the animation file path for this given shot."""
+        return self.file_path_format.format_map(
+            {'production': production, 'shot': shot, 'task_type': "anim"}
+        )
+
+    def get_lighting_file_path(self, production: Production, shot: Shot) -> str:
+        """Get the lighting file path for this given shot."""
+        return self.file_path_format.format_map(
+            {'production': production, 'shot': shot, 'task_type': "lighting"}
+        )
 
     def get_output_collection_name(self, shot: Shot, task_type: str) -> str:
+        """Get the collection name where the output is stored."""
+        return f"{shot.name}.{task_type}.output"
+
+    def is_valid(self) -> bool:
+        """Check if this shot contains all data, so it could be selected
+        for shot building.
         """
-        Get the collection name where the output is stored.
-        """
-        return f"{shot.sequence_code}_{shot.code}.{task_type}.output"
+        if not super().is_valid():
+            return False
+        return True
+
+    # Assuming path to file is in `project_name/svn/pro/shot/sequence_name/shot_name`
+    # Render Ouput path should be `project_name/shared/shot_frames/sequence_name/shot_name/`
+
+    def get_render_output_dir(self) -> str:
+        return f"//../../../../../shared/shot_frames/{self.sequence_code}/{self.name}/{self.name}.lighting"
+
+    def get_comp_output_dir(self) -> str:
+        return f"//../../../../../shared/shot_frames/{self.sequence_code}/{self.name}/{self.name}.comp"
 
 
-class Sequence_0002(SpriteFrightShot):
-    sequence_code = "0002"
-
-
-class Shot_0001_0001_A(Sequence_0002):
-    name = "001"
-    code = "0001"
+class GenericShot(ProductionShot):
+    is_generic = True
