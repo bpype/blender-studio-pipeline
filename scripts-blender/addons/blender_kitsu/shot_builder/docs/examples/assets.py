dad1f577331b5721b842fff21b8e628e64d32fed
Nick Alberelli <nick@tinymedia.ca>
2023-09-27 14:02:17 +0200

    Blender Kitsu: Fix Shot Builder example `assets.py`

--- scripts-blender/addons/blender_kitsu/shot_builder/docs/examples/assets.py
+++ scripts-blender/addons/blender_kitsu/shot_builder/docs/examples/assets.py
@@ -1,83 +1,30 @@
 from blender_kitsu.shot_builder.asset import Asset
 
 
-class SpriteFrightAsset(Asset):
-    path = "{production.path}/lib/{asset.asset_type}/{asset.code}/{asset.code}.blend"
+class ProductionAsset(Asset):
+    path = "{production.path}/lib/{asset.asset_type}/{asset.code}/{asset.code}.blend"  # Path to most assets
+    color_tag = "NONE"
 
 
-class Character(SpriteFrightAsset):
+# Categories
+class Character(ProductionAsset):
     asset_type = "char"
-    collection = "CH-{asset.code}"
+    collection = "CH-{asset.code}"  # Prefix for characters
 
 
-class Ellie(Character):
-    name = "Ellie"
-    code = "ellie"
-
-
-class Victoria(Character):
-    name = "Victoria"
-    code = "victoria"
-
-
-class Phil(Character):
-    name = "Phil"
-    code = "phil"
-
-
-class Rex(Character):
-    name = "Rex"
-    code = "rex"
-
-
-class Jay(Character):
-    name = "Jay"
-    code = "jay"
-
-# TODO: Bird character has no asset file yet.
-# class Bird(Character):
-#     name = "Bird"
-#     code = "bird"
-
-
-class Prop(SpriteFrightAsset):
+class Prop(ProductionAsset):
     asset_type = "props"
-    collection = "PR-{asset.code}"
+    collection = "PR-{asset.code}"  # Prefix for props
 
 
-class Boombox(Prop):
-    name = "Boombox"
-    code = "boombox"
+# Assets
+class MyCharacter(Character):
+    name = "My Character"  # Name on Kitsu Server
+    code = "mycharacter"  # Name of Collection without prefix (e.g. CH-mycharacter)
+    path = "{production.path}/lib/{asset.asset_type}/mycharacter/publish/mycharacter.v001.blend"  # This asset has a custom path
+    color_tag = "COLOR_01"
 
 
-class BBQGrill(Prop):
-    name = "BBQ Grill"
-    code = "bbq_grill"
-
-
-# NOTE: NotepadAndPencil is a combined asset. In Kitsu it is defined as a single asset. In the production
-# reportitory it is stored as 2 collections in a single file. See `hooks.link_char_prop_for_anim`
-# where this is handled.
-class NotepadAndPencil(Prop):
-    name = "Notepad and pencil"
-    code = "notepad_pencil"
-
-
-class Binoculars(Prop):
-    name = "Binoculars (Ellie)"
-    code = "binoculars"
-
-
-class Backpack(Prop):
-    name = "Backpack (Phil)"
-    code = "backpack"
-
-
-class Set(SpriteFrightAsset):
-    asset_type = "sets"
-    collection = "SE-{asset.code}"
-
-
-class MushroomGrove(Set):
-    name = "Mushroom grove"
-    code = "mushroom_grove"
+class MyProp(Prop):
+    name = "MyProp"
+    code = "myprop"
