67997b85466a10b639f02e862c143b9d63205fec
Sebastian Parborg <darkdefende@gmail.com>
2023-09-29 17:46:33 +0200

    project-tools: Make desktop icon file configurable

--- scripts/project-tools/install_desktop_file.sh
+++ scripts/project-tools/install_desktop_file.sh
@@ -6,6 +6,7 @@ cd "$(dirname "$0")"
 PROJECT_NAME="$(basename $(realpath ../../))"
 DESKTOP_FILE_DST="$HOME/.local/share/applications/blender_$PROJECT_NAME.desktop"
 BLENDER_BIN_PATH=$(realpath ./run_blender.py)
+BLENDER_ICON_NAME="blender-bin"
 
 echo "
 [Desktop Entry]
@@ -13,7 +14,7 @@ Name=Blender $PROJECT_NAME
 GenericName=3D modeler
 Keywords=3d;cg;modeling;animation;painting;sculpting;texturing;video editing;video tracking;rendering;render engine;cycles;game engine;python;
 Exec=sh -c '$BLENDER_BIN_PATH %f || read -p \"Press any key to close this window\"'
-Icon=blender
+Icon=$BLENDER_ICON_NAME
 Terminal=true
 Type=Application
 PrefersNonDefaultGPU=true
