e7ce597897dbd16f1102390b091b917616916473
Sebastian Parborg <darkdefende@gmail.com>
2023-09-29 15:27:02 +0200

    project-tools: Tweak the install_desktop_file script

--- scripts/project-tools/install_desktop_file.sh
+++ scripts/project-tools/install_desktop_file.sh
@@ -3,20 +3,20 @@
 # Make sure we are in this files directory
 cd "$(dirname "$0")"
 
-PROJECT_NAME="Pets"
+PROJECT_NAME="$(basename $(realpath ../../))"
 DESKTOP_FILE_DST="$HOME/.local/share/applications/blender_$PROJECT_NAME.desktop"
 BLENDER_BIN_PATH=$(realpath ./run_blender.py)
 
-cp ../../local/blender/linux/blender.desktop $DESKTOP_FILE_DST
-
-# Update the .desktop file data
-# 1. Replace the executable path with the path to "run_blender.py"
-# 2. Replace the desktop entry text with "Blender <PROJECT_NAME>"
-# 3. Make it so that you can right click on other blender binaries and "open with" the run_blender.py for this project
-# 4. Make sure we start Blender in a terminal window
-sed -i \
-  -e "s:Exec=blender:Exec=$BLENDER_BIN_PATH:" \
-  -e "s:Blender:Blender $PROJECT_NAME:" \
-  -e "s:application/x-blender:application/x-executable" \
-  -e "s:Terminal=false:Terminal=true" \
-  "$DESKTOP_FILE_DST"
+echo "
+[Desktop Entry]
+Name=Blender $PROJECT_NAME
+GenericName=3D modeler
+Keywords=3d;cg;modeling;animation;painting;sculpting;texturing;video editing;video tracking;rendering;render engine;cycles;game engine;python;
+Exec=sh -c '$BLENDER_BIN_PATH %f || read -p \"Press any key to close this window\"'
+Icon=blender
+Terminal=true
+Type=Application
+PrefersNonDefaultGPU=true
+Categories=Graphics;3DGraphics;
+MimeType=application/x-executable;
+" > $DESKTOP_FILE_DST
