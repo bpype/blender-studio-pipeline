fcfc6ad52a646af6f2afb79265192db636308183
Márton Lente <lentemarton@gmail.com>
2023-11-15 19:19:44 +0100

    UI: Override default theme config and style for web-assets

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -23,8 +23,11 @@ export default defineConfig({
   ],
   themeConfig: {
     logo: {
+      /*
+      Logo is injected from Vue component LayoutNavBarGlobal
       light: '/blender-studio-logo-black.svg',
       dark: '/blender-studio-logo-white.svg'
+      */
     },
     siteTitle: false,
     footer: {
@@ -36,14 +39,16 @@ export default defineConfig({
     search: {
       provider: 'local'
     },
-    // TODO: cleanup redundant config nav if nav-global is ok
     // https://vitepress.dev/reference/default-theme-config
     nav: [
+      /*
+      Nav is injected from Vue component LayoutNavBarGlobal
       { text: 'Films', link: `${studioURL}/films` },
-      { text: 'Training', link: `${studioURL}/training` },
-      { text: 'Blog', link: `${studioURL}/blog` },
-      { text: 'Pipeline', link: '/' },
-      { text: 'Characters', link: `${studioURL}/characters`, }
+            { text: 'Training', link: `${studioURL}/training` },
+            { text: 'Blog', link: `${studioURL}/blog` },
+            { text: 'Pipeline', link: '/' },
+            { text: 'Characters', link: `${studioURL}/characters`, }
+      */
     ],
 
     sidebar: [
