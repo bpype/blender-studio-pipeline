0629b51f1506f7237a7bbd3c037b7979f9177134
Nick Alberelli <nick@tinymedia.ca>
2024-02-02 10:56:54 -0500

    Blender Kitsu: Add Debug Print to Shot Builder

--- scripts-blender/addons/blender_kitsu/shot_builder/file_save.py
+++ scripts-blender/addons/blender_kitsu/shot_builder/file_save.py
@@ -2,10 +2,11 @@ from pathlib import Path
 import bpy
 
 
-def save_shot_builder_file(file_path: str)->bool:
+def save_shot_builder_file(file_path: str) -> bool:
     """Save Shot File within Folder of matching name.
     Set Shot File to relative Paths."""
     if Path(file_path).exists():
+        print(f"Shot Builder cannot overwrite existing file '{file_path}'")
         return False
     dir_path = Path(file_path).parent
     dir_path.mkdir(parents=True, exist_ok=True)
