fd8c0475fb508774cc65ca6d8d5d5c295beda23b
Demeter Dzadik <demeterdzadik@gmail.com>
2025-07-25 19:57:43 +0200

    Asset P: Clean up transfer_constraint()
    There's no need to consider order since all constraints get nuked and copied from a single source in order from top to bottom.
    There is no need to prefix them either, since all constraints are owned by one owner (Parenting)

--- scripts-blender/addons/asset_pipeline/merge/transfer_data/transfer_functions/constraints.py
+++ scripts-blender/addons/asset_pipeline/merge/transfer_data/transfer_functions/constraints.py
@@ -8,9 +8,8 @@ from ..transfer_util import (
     transfer_data_item_is_missing,
     check_transfer_data_entry,
 )
-from ...naming import task_layer_prefix_name_get, task_layer_prefix_basename_get
+from ...naming import task_layer_prefix_name_get
 from .transfer_function_util.drivers import transfer_drivers, cleanup_drivers
-from .transfer_function_util.visibility import override_obj_visibility
 from ...task_layer import get_transfer_data_owner
 from .... import constants, logging
 
@@ -57,53 +56,31 @@ def init_constraints(scene, obj):
 def transfer_constraint(constraint_name, target_obj, source_obj):
     logger = logging.get_logger()
     context = bpy.context
-    # remove old and sync existing modifiers
-    old_mod = target_obj.constraints.get(constraint_name)
-    if old_mod:
-        target_obj.constraints.remove(old_mod)
+    # Remove old and sync existing constraints.
+    old_con = target_obj.constraints.get(constraint_name)
+    if old_con:
+        target_obj.constraints.remove(old_con)
 
-    source_index = 0
-    source_constraint = None
-    # transfer new modifiers
-    for source_index, source_constraint in enumerate(source_obj.constraints):
-        if source_constraint.name == constraint_name:
-            source_index = source_index
-            break
-
-    if not source_constraint:
-        logger.debug(
-            f"Constraint Transfer cancelled, '{constraint_name}' not found on '{source_obj.name}'"
-        )
+    src_idx = source_obj.constraints.find(constraint_name)
+    if src_idx == -1:
         # This happens if a modifier's transfer data is still around, but the modifier
         # itself was removed.
+        logger.debug(f"Constraint Transfer cancelled, '{constraint_name}' not found on '{source_obj.name}'")
         return
 
-    constraint_new = target_obj.constraints.new(source_constraint.type)
-    constraint_new.name = source_constraint.name
-    # sort new modifier at correct index (default to beginning of the stack)
-    idx = 0
-    if source_index > 0:
-        name_prev = source_obj.constraints[source_index - 1].name
-        for target_mod_i, target_constraint in enumerate(target_obj.constraints):
-            if task_layer_prefix_basename_get(
-                target_constraint.name
-            ) == task_layer_prefix_basename_get(name_prev):
-                idx = target_mod_i + 1
+    src_con = source_obj.constraints[src_idx]
+    new_con = target_obj.constraints.new(src_con.type)
+    new_con.name = src_con.name
 
-    with override_obj_visibility(obj=target_obj, scene=context.scene):
-        with context.temp_override(object=target_obj):
-            bpy.ops.constraint.move_to_index(constraint=constraint_new.name, index=idx)
-
-    constraint_target = target_obj.constraints.get(source_constraint.name)
-    props = [p.identifier for p in source_constraint.bl_rna.properties if not p.is_readonly]
+    props = [p.identifier for p in src_con.bl_rna.properties if not p.is_readonly]
     for prop in props:
-        value = getattr(source_constraint, prop)
-        setattr(constraint_target, prop, value)
+        value = getattr(src_con, prop)
+        setattr(new_con, prop, value)
 
-    # HACK to cover edge case of armature constraints
-    if source_constraint.type == "ARMATURE":
-        for target_item in source_constraint.targets:
-            new_target = constraint_new.targets.new()
+    # Armature constraints have some nested properties we need to copy...
+    if src_con.type == "ARMATURE":
+        for target_item in src_con.targets:
+            new_target = new_con.targets.new()
             new_target.target = target_item.target
             new_target.subtarget = target_item.subtarget
 
